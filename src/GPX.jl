4ed33ad6d7ebb5de27aaf7ca9f7a49a037ff0d3c
AbelHo <21101710+AbelHo@users.noreply.github.com>
2023-08-03 23:07:38 +0800

    fix(GPX): filename & tz passing

--- src/GPX.jl
+++ src/GPX.jl
@@ -7,7 +7,7 @@ import Geodesy: LLA
 import TimeZones: astimezone, localzone
 import PooledArrays: PooledArray
 
-function gpx_segment2pts(segment; tz=localzone())
+function gpx_segment2pts(segment; filename=filename, tz=localzone())
     p = segment.points
     df = DataFrame(time=[astimezone(p1.time, tz) for p1 ∈ p], location=[LLA(p1.lat, p1.lon) for p1 ∈ p])
     df.filename = repeat(PooledArray([filename]), size(df, 1))
@@ -24,9 +24,9 @@ function read(filename; tz=localzone())
   end
   gpx = GPX.read_gpx_file(filename)
   length(gpx.tracks) > 1 && throw(ErrorException("More than one tracks in GPX file"))
-  length(gpx.tracks[1].segments) > 1 && return gpx_segment2pts.(gpx.tracks[1].segments; tz=tz)
+  length(gpx.tracks[1].segments) > 1 && return gpx_segment2pts.(gpx.tracks[1].segments; filename=filename, tz=tz)
 
-  gpx_segment2pts(gpx.tracks[1].segments[1])
+  gpx_segment2pts(gpx.tracks[1].segments[1]; filename=filename, tz=tz)
 end
 
 function read(filenames::AbstractVector)
